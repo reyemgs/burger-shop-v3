d83fd7b5f928b6c20352fd11a4ac3a643a76620b
reyemgs <thetortyan@gmail.com>
2020-11-30 16:09:25 +0300

    improvement: EventEmitter with Map
    EventHandler class has been replaced by EventEmitter, the implementation is now based on Map, not an empty object

--- src/js/components/EventHandler.js
+++ src/js/components/EventHandler.js
@@ -1,29 +1,22 @@
-export default class EventHandler {
+export default class EventEmitter {
     constructor() {
-        this.events = {};
+        this.events = new Map();
     }
 
     on(eventName, fn) {
-        this.events[eventName] = this.events[eventName] || [];
-        this.events[eventName].push(fn);
+        if (this.events.has(eventName)) return;
+        this.events.set(eventName, fn);
     }
 
-    off(eventName, fn) {
-        if (this.events[eventName]) {
-            for (let i = 0; i < this.events[eventName].length; i++) {
-                if (this.events[eventName][i] === fn) {
-                    this.events[eventName].splice(i, 1);
-                    break;
-                }
-            }
+    off(eventName) {
+        if (this.events.has(eventName)) {
+            this.events.delete(eventName);
         }
     }
 
     emit(eventName, data) {
-        if (this.events[eventName]) {
-            this.events[eventName].forEach(fn => {
-                fn(data);
-            });
+        if (this.events.has(eventName)) {
+            this.events.get(eventName)(data);
         }
     }
 }
