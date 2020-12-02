e77ce9d99e9734e7f9c987c3802bc0c88bb70596
reyemgs <thetortyan@gmail.com>
2020-12-02 12:04:03 +0300

    improvement: EventEmitter with caller object

--- src/js/components/EventEmitter.js
+++ src/js/components/EventEmitter.js
@@ -3,20 +3,34 @@ export default class EventEmitter {
         this.events = new Map();
     }
 
-    on(eventName, fn) {
-        if (this.events.has(eventName)) return;
-        this.events.set(eventName, fn);
+    on(eventName, fn, object) {
+        if (this.events.has(eventName)) {
+            const listeners = this.events.get(eventName);
+            listeners.push({ object: object, fn: fn });
+        } else {
+            this.events.set(eventName, []);
+            const listeners = this.events.get(eventName);
+            listeners.push({ object: object, fn: fn });
+        }
+        console.log(this.events.get(eventName));
     }
 
-    off(eventName) {
+    off(eventName, object) {
         if (this.events.has(eventName)) {
-            this.events.delete(eventName);
+            const listeners = this.events.get(eventName);
+            for (let i = 0; i < listeners.length; i++) {
+                if (listeners[i].object === object) {
+                    listeners.splice(i, 1);
+                    console.log(this.events.get(eventName));
+                    break;
+                }
+            }
         }
     }
 
     emit(eventName, data) {
         if (this.events.has(eventName)) {
-            this.events.get(eventName)(data);
+            this.events.get(eventName).forEach(instanse => instanse.fn(data));
         }
     }
 }
