7acea8ae51c9ef4ee0263546ddfdf45c1db0169f
reyemgs <thetortyan@gmail.com>
2020-12-04 11:58:09 +0300

    style: minor style

--- src/js/components/EventEmitter.js
+++ src/js/components/EventEmitter.js
@@ -7,19 +7,24 @@ export default class EventEmitter {
         if (!this.events.has(eventName)) {
             this.events.set(eventName, []);
         }
+
         const listeners = this.events.get(eventName);
         listeners.push(fn);
     }
 
     off(eventName, fn) {
         if (!this.events.has(eventName)) return;
+
         const listeners = this.events.get(eventName);
+
         for (let i = 0; i < listeners.length; i++) {
             if (listeners[i] === fn) {
                 listeners.splice(i, 1);
+
                 if (listeners.length === 0) {
                     this.events.delete(eventName);
                 }
+
                 break;
             }
         }
